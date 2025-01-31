6651a90b7d6b6c447b5fd305d6b77a0a2c493728
abdullah1655 <ALYMAHDY69@GMAIL.COM>
2025-01-31 04:51:28 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,9 @@
 return {
-    badge_item_name = 'stevo_policebadge',
+    badge_item_name = 'abdullah_policebadge',
 
-    job_names = {'police', 'sheriff'}, -- Police job names.
+    job_names = {'police', 'cadet'}, -- Police job names.
     badge_show_time = 5000, -- Time badge should display
 
-    set_image_command = 'setbadgephoto', -- Command to change badge photo
 
 
 }
